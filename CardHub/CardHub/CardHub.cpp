f014968aa7dc202eceae84f8c93fc7f1d9c62ae3
Alex M <153501077+D31TO@users.noreply.github.com>
2024-02-19 11:57:43 +0000

    Hit function (Reall bad)
    Made a really over the top and confusing hit function that somehow works (Really badly)

--- CardHub/CardHub/CardHub.cpp
+++ CardHub/CardHub/CardHub.cpp
@@ -6,6 +6,8 @@
 #include <random>
 #include <algorithm>
 #include "Deck.h"
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 //Declerations
@@ -31,6 +33,13 @@ int PlayerVal4;
 string PlayerSuit4;
 int PlayerVal5;
 string PlayerSuit5;
+int DealerVal3;
+string DealerSuit3;
+int DealerVal4;
+string DealerSuit4;
+int DealerVal5;
+string DealerSuit5;
+
 
 //vector<string> SNames = { "S", "H", "C", "D" };
 
@@ -52,20 +61,6 @@ static string CardNames[] = {
 
 int main()
 {
-
-	//for (int i = 1; i < 14; i++) {
-	//	for (int j = 0; j < 4; j++) {
-	//		Card NewCard = Card(SNames[j], i);
-	//		Deck.push_back(NewCard);
-	//	}
-	//}
-
-	//random_device rd;
-	//mt19937 g(rd());
-
-	//shuffle(Deck.begin(), Deck.end(), g);
-
-
 	Deck TheDeck = Deck();
 	TheDeck.Shuffle();
 
@@ -84,14 +79,18 @@ int main()
 		//Fetching cards
 		PlayerVal1 = TheDeck.GetCard(0).getValue();
 		PlayerSuit1 = TheDeck.GetCard(0).getSuit();
+
 		DealerVal1 = TheDeck.GetCard(1).getValue();
 		DealerSuit1 = TheDeck.GetCard(1).getSuit();
+
 		PlayerVal2 = TheDeck.GetCard(2).getValue();
 		PlayerSuit2 = TheDeck.GetCard(2).getSuit();
+
 		DealerVal2 = TheDeck.GetCard(3).getValue();
 		DealerSuit2 = TheDeck.GetCard(3).getSuit();
 
-		vector<Card> PlayerHand = { TheDeck.GetCard(0), TheDeck.GetCard(2)};
+
+		vector<Card> PlayerHand = { TheDeck.GetCard(0), TheDeck.GetCard(2) };
 		vector<Card> DealerHand = { TheDeck.GetCard(1), TheDeck.GetCard(3) };
 
 		int PlayerTotal = 0;
@@ -101,20 +100,20 @@ int main()
 		//Acces and total for Player
 		for (Card NextCard : PlayerHand) {
 			int Value = NextCard.getValue();
-		
+
 			if (Value == 1) {
 				NumAces++;
-				
+
 			}
 			else {
-				
+
 				if (Value > 10) {
 					PlayerTotal += 10;
-					
+
 				}
 				else {
 					PlayerTotal += Value;
-					
+
 				}
 			}
 		}
@@ -165,7 +164,7 @@ int main()
 
 		//Player first values
 		cout << "You have ";
-		if (PlayerVal1 == 1){
+		if (PlayerVal1 == 1) {
 			cout << "A of ";
 			cout << PlayerSuit1 << " ";
 		}
@@ -282,23 +281,461 @@ int main()
 		int pdec;
 		cin >> pdec;
 
+
+		//Hit 1
 		if (pdec == 1) {
 			cout << "You hit" << endl;
+			PlayerTotal = 0;
+			PlayerVal3 = TheDeck.GetCard(4).getValue();
+			PlayerSuit3 = TheDeck.GetCard(4).getSuit();
+			vector<Card> PlayerHand = { TheDeck.GetCard(0), TheDeck.GetCard(2), TheDeck.GetCard(4) };
+
+			for (Card NextCard : PlayerHand) {
+				int Value = NextCard.getValue();
+
+				if (Value == 1) {
+					NumAces++;
+
+				}
+				else {
+
+					if (Value > 10) {
+						PlayerTotal += 10;
+
+					}
+					else {
+						PlayerTotal += Value;
+
+					}
+				}
+			}
+			if (NumAces > 0) {
+				for (int i = 0; i < NumAces; i++) {
+					if (PlayerTotal > 21) {
+						PlayerTotal += 1;
+					}
+					else {
+						PlayerTotal += 11;
+					}
+				}
+			}
+			NumAces = 0;
+
+			//Player first values
+			cout << "You have ";
+			if (PlayerVal1 == 1) {
+				cout << "A of ";
+				cout << PlayerSuit1 << " ";
+			}
+			else if (PlayerVal1 == 11) {
+				cout << "J of ";
+				cout << PlayerSuit1 << " ";
+			}
+			else if (PlayerVal1 == 12) {
+				cout << "Q of ";
+				cout << PlayerSuit1 << " ";
+			}
+			else if (PlayerVal1 == 13) {
+				cout << "K of ";
+				cout << PlayerSuit1 << " ";
+			}
+			else {
+				cout << PlayerVal1 << " of " << PlayerSuit1;
+			}
+
+			cout << " and a ";
+
+			//Player second values
+			if (PlayerVal2 == 1) {
+				cout << "A of ";
+				cout << PlayerSuit2;
+			}
+			else if (PlayerVal2 == 11) {
+				cout << "J of ";
+				cout << PlayerSuit2;
+			}
+			else if (PlayerVal2 == 12) {
+				cout << "Q of ";
+				cout << PlayerSuit2;
+			}
+			else if (PlayerVal2 == 13) {
+				cout << "K of ";
+				cout << PlayerSuit2;
+			}
+			else {
+				cout << PlayerVal2 << " of " << PlayerSuit2;
+			}
+			cout << " and a ";
+
+			//Player third values
+			if (PlayerVal3 == 1) {
+				cout << "A of ";
+				cout << PlayerSuit3;
+			}
+			else if (PlayerVal3 == 11) {
+				cout << "J of ";
+				cout << PlayerSuit3;
+			}
+			else if (PlayerVal3 == 12) {
+				cout << "Q of ";
+				cout << PlayerSuit3;
+			}
+			else if (PlayerVal3 == 13) {
+				cout << "K of ";
+				cout << PlayerSuit3;
+			}
+			else {
+				cout << PlayerVal3 << " of " << PlayerSuit3;
+			}
+
+			cout << endl;
+
+			cout << "Your total = " << PlayerTotal << endl;
+
+			if (PlayerTotal == 21) {
+				cout << "Well done! You win!" << endl;
+				exit(0);
+			}
+			else if (PlayerTotal > 21) {
+				cout << "You have gone bust, you lose." << endl;
+				exit(0);
+			}
+
+
+
+
+			else if (PlayerTotal < 21) {
+				//Player Decision 2
+				cout << "Would you like to 1) hit or 2) stand?" << endl;
+				int pdec;
+				cin >> pdec;
+				if (pdec == 1) {
+					cout << "You hit" << endl;
+					PlayerTotal = 0;
+					PlayerVal4 = TheDeck.GetCard(5).getValue();
+					PlayerSuit4 = TheDeck.GetCard(5).getSuit();
+					vector<Card> PlayerHand = { TheDeck.GetCard(0), TheDeck.GetCard(2), TheDeck.GetCard(4), TheDeck.GetCard(5) };
+
+					for (Card NextCard : PlayerHand) {
+						int Value = NextCard.getValue();
+
+						if (Value == 1) {
+							NumAces++;
+
+						}
+						else {
+
+							if (Value > 10) {
+								PlayerTotal += 10;
+
+							}
+							else {
+								PlayerTotal += Value;
+
+							}
+						}
+					}
+					if (NumAces > 0) {
+						for (int i = 0; i < NumAces; i++) {
+							if (PlayerTotal > 21) {
+								PlayerTotal += 1;
+							}
+							else {
+								PlayerTotal += 11;
+							}
+						}
+					}
+					NumAces = 0;
+
+					//Player first values
+					cout << "You have ";
+					if (PlayerVal1 == 1) {
+						cout << "A of ";
+						cout << PlayerSuit1 << " ";
+					}
+					else if (PlayerVal1 == 11) {
+						cout << "J of ";
+						cout << PlayerSuit1 << " ";
+					}
+					else if (PlayerVal1 == 12) {
+						cout << "Q of ";
+						cout << PlayerSuit1 << " ";
+					}
+					else if (PlayerVal1 == 13) {
+						cout << "K of ";
+						cout << PlayerSuit1 << " ";
+					}
+					else {
+						cout << PlayerVal1 << " of " << PlayerSuit1;
+					}
+
+					cout << " and a ";
+
+					//Player second values
+					if (PlayerVal2 == 1) {
+						cout << "A of ";
+						cout << PlayerSuit2;
+					}
+					else if (PlayerVal2 == 11) {
+						cout << "J of ";
+						cout << PlayerSuit2;
+					}
+					else if (PlayerVal2 == 12) {
+						cout << "Q of ";
+						cout << PlayerSuit2;
+					}
+					else if (PlayerVal2 == 13) {
+						cout << "K of ";
+						cout << PlayerSuit2;
+					}
+					else {
+						cout << PlayerVal2 << " of " << PlayerSuit2;
+					}
+					cout << " and a ";
+
+					//Player third values
+					if (PlayerVal3 == 1) {
+						cout << "A of ";
+						cout << PlayerSuit3;
+					}
+					else if (PlayerVal3 == 11) {
+						cout << "J of ";
+						cout << PlayerSuit3;
+					}
+					else if (PlayerVal3 == 12) {
+						cout << "Q of ";
+						cout << PlayerSuit3;
+					}
+					else if (PlayerVal3 == 13) {
+						cout << "K of ";
+						cout << PlayerSuit3;
+					}
+					else {
+						cout << PlayerVal3 << " of " << PlayerSuit3;
+					}
+
+					cout << " and a ";
+
+					//Player fourth values
+					if (PlayerVal4 == 1) {
+						cout << "A of ";
+						cout << PlayerSuit4;
+					}
+					else if (PlayerVal4 == 11) {
+						cout << "J of ";
+						cout << PlayerSuit4;
+					}
+					else if (PlayerVal4 == 12) {
+						cout << "Q of ";
+						cout << PlayerSuit4;
+					}
+					else if (PlayerVal4 == 13) {
+						cout << "K of ";
+						cout << PlayerSuit4;
+					}
+					else {
+						cout << PlayerVal4 << " of " << PlayerSuit4;
+					}
+
+					cout << endl;
+
+					cout << "Your total = " << PlayerTotal << endl;
+
+					if (PlayerTotal == 21) {
+						cout << "Well done! You win!" << endl;
+						exit(0);
+					}
+					else if (PlayerTotal > 21) {
+						cout << "You have gone bust, you lose." << endl;
+						exit(0);
+					}
+					else if (PlayerTotal < 21) {
+						//Player Decision 3
+						cout << "Would you like to 1) hit or 2) stand?" << endl;
+						int pdec;
+						cin >> pdec;
+						if (pdec == 1) {
+							cout << "You hit" << endl;
+							PlayerTotal = 0;
+							PlayerVal5 = TheDeck.GetCard(6).getValue();
+							PlayerSuit5 = TheDeck.GetCard(6).getSuit();
+							vector<Card> PlayerHand = { TheDeck.GetCard(0), TheDeck.GetCard(2), TheDeck.GetCard(4), TheDeck.GetCard(5), TheDeck.GetCard(6) };
+
+							for (Card NextCard : PlayerHand) {
+								int Value = NextCard.getValue();
+
+								if (Value == 1) {
+									NumAces++;
+
+								}
+								else {
+
+									if (Value > 10) {
+										PlayerTotal += 10;
+
+									}
+									else {
+										PlayerTotal += Value;
+
+									}
+								}
+							}
+							if (NumAces > 0) {
+								for (int i = 0; i < NumAces; i++) {
+									if (PlayerTotal > 21) {
+										PlayerTotal += 1;
+									}
+									else {
+										PlayerTotal += 11;
+									}
+								}
+							}
+							NumAces = 0;
+
+							//Player first values
+							cout << "You have ";
+							if (PlayerVal1 == 1) {
+								cout << "A of ";
+								cout << PlayerSuit1 << " ";
+							}
+							else if (PlayerVal1 == 11) {
+								cout << "J of ";
+								cout << PlayerSuit1 << " ";
+							}
+							else if (PlayerVal1 == 12) {
+								cout << "Q of ";
+								cout << PlayerSuit1 << " ";
+							}
+							else if (PlayerVal1 == 13) {
+								cout << "K of ";
+								cout << PlayerSuit1 << " ";
+							}
+							else {
+								cout << PlayerVal1 << " of " << PlayerSuit1;
+							}
+
+							cout << " and a ";
+
+							//Player second values
+							if (PlayerVal2 == 1) {
+								cout << "A of ";
+								cout << PlayerSuit2;
+							}
+							else if (PlayerVal2 == 11) {
+								cout << "J of ";
+								cout << PlayerSuit2;
+							}
+							else if (PlayerVal2 == 12) {
+								cout << "Q of ";
+								cout << PlayerSuit2;
+							}
+							else if (PlayerVal2 == 13) {
+								cout << "K of ";
+								cout << PlayerSuit2;
+							}
+							else {
+								cout << PlayerVal2 << " of " << PlayerSuit2;
+							}
+							cout << " and a ";
+
+							//Player third values
+							if (PlayerVal3 == 1) {
+								cout << "A of ";
+								cout << PlayerSuit3;
+							}
+							else if (PlayerVal3 == 11) {
+								cout << "J of ";
+								cout << PlayerSuit3;
+							}
+							else if (PlayerVal3 == 12) {
+								cout << "Q of ";
+								cout << PlayerSuit3;
+							}
+							else if (PlayerVal3 == 13) {
+								cout << "K of ";
+								cout << PlayerSuit3;
+							}
+							else {
+								cout << PlayerVal3 << " of " << PlayerSuit3;
+							}
+
+							cout << " and a ";
+
+							//Player fourth values
+							if (PlayerVal4 == 1) {
+								cout << "A of ";
+								cout << PlayerSuit4;
+							}
+							else if (PlayerVal4 == 11) {
+								cout << "J of ";
+								cout << PlayerSuit4;
+							}
+							else if (PlayerVal4 == 12) {
+								cout << "Q of ";
+								cout << PlayerSuit4;
+							}
+							else if (PlayerVal4 == 13) {
+								cout << "K of ";
+								cout << PlayerSuit4;
+							}
+							else {
+								cout << PlayerVal4 << " of " << PlayerSuit4;
+							}
+
+							cout << " and a ";
+
+							//Player fifth values
+							if (PlayerVal5 == 1) {
+								cout << "A of ";
+								cout << PlayerSuit5;
+							}
+							else if (PlayerVal5 == 11) {
+								cout << "J of ";
+								cout << PlayerSuit5;
+							}
+							else if (PlayerVal5 == 12) {
+								cout << "Q of ";
+								cout << PlayerSuit5;
+							}
+							else if (PlayerVal5 == 13) {
+								cout << "K of ";
+								cout << PlayerSuit5;
+							}
+							else {
+								cout << PlayerVal5 << " of " << PlayerSuit5;
+							}
+
+							cout << endl;
+
+							cout << "Your total = " << PlayerTotal << endl;
+
+							if (PlayerTotal == 21) {
+								cout << "Well done! You win!" << endl;
+								exit(0);
+							}
+							else if (PlayerTotal > 21) {
+								cout << "You have gone bust, you lose." << endl;
+								exit(0);
+							}
+						}
+					}
+
+				}
+			}
 
 		}
 
 
 		if (pdec == 2) {
 			cout << "You stand" << endl;
+
 		}
 
+		else if (inp == 2) {
+			cout << "Loading Solitaire" << endl;
+		}
 
 	}
-	else if (inp == 2) {
-		cout << "Loading Solitaire" << endl;
-	}
-
-
 
 }
 
